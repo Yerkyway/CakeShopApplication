b7f7dabab76213d97244a8f37db3cab8d0d63cd5
Yerkebulan <yerkeshhhh@gmail.com>
2024-04-16 14:00:06 +0500

    Initial commit

---
+++ src/main/java/com/sheryians/major/model/Category.java
@@ -0,0 +1,16 @@
+package com.sheryians.major.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    private Integer id;
+    private String name;
+
+}
